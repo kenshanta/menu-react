77ebbcbf22b134260cb2bf1fea90538418e02be4
Sarkis Kovlekjian <kovlekjian@Sarkiss-MacBook-Pro.local>
2023-07-27 16:44:06 +0200

    [bugfix] update package.json for swipable-views

--- src/App.tsx
+++ src/App.tsx
@@ -3,50 +3,40 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Routes } from "./modules";
 import { ThemePrimary } from "./assets/theme";
 import { ThemeProvider } from "@material-ui/styles";
-// import { useMenuStore } from "./stores/menuStore";
-// import { MenuService } from "./services";
+import { useMenuStore } from "./stores/menuStore";
+import { MenuService } from "./services";
 import { PanelSkeleton } from "./components/panelBody";
 
 const App: React.FC = () => {
-  // const {
-  //   localeLanguage,
-  //   setLocaleLanguage,
-  //   setLocaleCategoriesList,
-  //   setLocaleSubCategories,
-  //   setCurrentCategoriesList,
-  //   currentCategoriesList,
-  // } = useMenuStore();
+  const {
+    localeLanguage,
+    setLocaleCategoriesList,
+    setCurrentCategoriesList,
+  } = useMenuStore();
 
-  // React.useEffect(() => {
-  //   const fetchCategoriesList = async () => {
-  //     const productItemsList = await MenuService.getCategoriesByLocale("all");
-  //     setLocaleCategoriesList(productItemsList);
-  //     logicalOperator(productItemsList);
-  //   };
-  //   const logicalOperator = (data: any) => {
-  //     let categoryListArr: any[] = [];
-  //     data.map((category) => {
-  //       if (localeLanguage === "en" && category.locale === "en") {
-  //         categoryListArr.push(category);
-  //       } else if (localeLanguage === "hy-AM" && category.locale === "hy-AM") {
-  //         categoryListArr.push(category);
-  //       }
-  //       return categoryListArr;
-  //     });
-  //     categoryListArr.sort(function (a, b) {
-  //       return a.id - b.id;
-  //     });
-  //     setCurrentCategoriesList(categoryListArr);
-  //   };
-  //   fetchCategoriesList();
-  // }, [
-  //   localeLanguage,
-  //   setLocaleCategoriesList,
-  //   setLocaleLanguage,
-  //   setLocaleSubCategories,
-  //   setCurrentCategoriesList,
-  //   currentCategoriesList,
-  // ]);
+  React.useEffect(() => {
+    const fetchCategoriesList = async () => {
+      const productItemsList = await MenuService.getCategoriesByLocale("all");
+      setLocaleCategoriesList(productItemsList);
+      logicalOperator(productItemsList);
+    };
+    const logicalOperator = (data: any) => {
+      let categoryListArr: any[] = [];
+      data.map((category) => {
+        if (localeLanguage === "en" && category.locale === "en") {
+          categoryListArr.push(category);
+        } else if (localeLanguage === "hy-AM" && category.locale === "hy-AM") {
+          categoryListArr.push(category);
+        }
+        return categoryListArr;
+      });
+      categoryListArr.sort(function (a, b) {
+        return a.id - b.id;
+      });
+      setCurrentCategoriesList(categoryListArr);
+    };
+    fetchCategoriesList();
+  });
 
   return (
     <Suspense fallback={<PanelSkeleton />}>
