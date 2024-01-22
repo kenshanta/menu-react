303f405fc0e968c116e74bd5f4526d5409ebf175
Sarkis Kovlekjian <kovlekjian@Sarkiss-MacBook-Pro.local>
2024-01-22 01:27:23 +0100

    additional armenian mock data

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.scss";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+const container = document.getElementById("container");
+const root = createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("container")
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
